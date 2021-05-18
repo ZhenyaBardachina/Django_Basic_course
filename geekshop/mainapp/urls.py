d36705dd1dd80aeb153c6d7a4609ff334560c01d
Zhenya <zhenya-bogdasheva@ya.ru>
2021-05-18 21:21:23 +0300

    empty_dir

--- geekshop/mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import category
-from .views import products
-
-urlpatterns = [
-    path('', products, name='products'),
-    path('category/<int:pk>/', category, name='category'),
-]
